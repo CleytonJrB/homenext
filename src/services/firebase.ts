af2ee96fd99b28a308ab4afa20f1a92dd22a2fc6
CleytonJrB <cleytonjrb@outlook.com>
2021-07-27 14:58:09 -0300

    firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_APP_API_KEY,
+  authDomain: process.env.NEXT_APP_AUTH_DOMAIN,
+  projectId: process.env.NEXT_APP_PROJECT_ID,
+  storageBucket: process.env.NEXT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_APP_APP_ID
+};
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth();
+export const database = firebase.database();
